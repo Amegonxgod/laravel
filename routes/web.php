7641b3164559095b1a4a9c93f34a0e25fc63fb45
Amegonxgod <irotmarchen@gmail.com>
2025-02-22 20:14:38 +0300

    id

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\UserController;
+
+Route::get('/user/{id?}', [UserController::class, 'show']);
